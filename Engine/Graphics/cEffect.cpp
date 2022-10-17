32fa7288eb4e88f5c943f3db96e2de0bd3427336
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-10-16 22:46:26 -0600

    Enable Depth Testing and Depth Writing

--- Engine/Graphics/cEffect.cpp
+++ Engine/Graphics/cEffect.cpp
@@ -95,8 +95,10 @@ eae6320::cResult eae6320::Graphics::cEffect::Initialize(const std::string& i_fra
 			uint8_t renderStateBits = 0;
 
 			eae6320::Graphics::RenderStates::DisableAlphaTransparency(renderStateBits);
-			eae6320::Graphics::RenderStates::DisableDepthTesting(renderStateBits);
-			eae6320::Graphics::RenderStates::DisableDepthWriting(renderStateBits);
+			/*eae6320::Graphics::RenderStates::DisableDepthTesting(renderStateBits);
+			eae6320::Graphics::RenderStates::DisableDepthWriting(renderStateBits);*/
+			eae6320::Graphics::RenderStates::EnableDepthTesting(renderStateBits);
+			eae6320::Graphics::RenderStates::EnableDepthWriting(renderStateBits);
 			eae6320::Graphics::RenderStates::DisableDrawingBothTriangleSides(renderStateBits);
 
 			return renderStateBits;
