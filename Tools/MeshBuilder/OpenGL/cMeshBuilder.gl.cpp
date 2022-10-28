f51bae91cc6c6b7e47f726f6ab85b6147c998f98
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-10-28 01:44:56 -0600

    Read the binary mesh file

--- Tools/MeshBuilder/OpenGL/cMeshBuilder.gl.cpp
+++ Tools/MeshBuilder/OpenGL/cMeshBuilder.gl.cpp
@@ -2,3 +2,17 @@
 //=========
 
 #include "../cMeshBuilder.h"
+
+eae6320::cResult eae6320::Assets::cMeshBuilder::ProcessIndexArray(uint16_t* i_indexArray, size_t i_indexArraySize)
+{
+	auto result = Results::Success;
+
+	for (size_t index = 0; index < i_indexArraySize; index += 3)
+	{
+		uint16_t cacheValue = i_indexArray[index + 2];
+		i_indexArray[index + 2] = i_indexArray[index + 1];
+		i_indexArray[index + 1] = cacheValue;
+	}
+
+	return result;
+}
