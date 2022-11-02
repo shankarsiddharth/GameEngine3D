0d7e68c20c3ad23ed276f5e84bb0cf16f0c2540e
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-11-02 00:19:24 -0600

    Create new project and add basic node structure

---
+++ Engine/Narrator/Runtime/NarratorRuntime/Core/StartNode.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Node.h"
+
+namespace Narrator
+{
+	namespace Runtime
+	{
+		class StartNode : public Node
+		{
+
+		public:
+			StartNode(); 
+			~StartNode();
+		};
+	}
+}
