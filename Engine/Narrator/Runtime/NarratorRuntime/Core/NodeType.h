d1b98af9d774908eb1d8848616dc1d6f90c1ff58
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-11-02 00:19:24 -0600

    Create new project and add basic node structure

---
+++ Engine/Narrator/Runtime/NarratorRuntime/Core/NodeType.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+
+namespace Narrator 
+{
+	namespace Runtime
+	{
+		enum class TNodeType : std::uint16_t
+		{
+			kNone = 0,
+			//Start Node
+			kStart,
+			//End Node
+			kEnd,
+			//Text Dialogue Content Node
+			kDialogue,
+			//Link Node
+			kDivert,
+			//Section Node
+			kKnots,
+			//Choice Node
+			kChoice,
+			//Decision Node
+			kDesicion
+		};
+	}
+}
---
+++ Engine/Narrator/Runtime/NarratorRuntime/Core/StartNode.cpp
@@ -0,0 +1,12 @@
+#include "StartNode.h"
+
+Narrator::Runtime::StartNode::StartNode()
+	:Narrator::Runtime::Node(TNodeType::kStart)
+{
+
+}
+
+Narrator::Runtime::StartNode::~StartNode()
+{
+
+}
