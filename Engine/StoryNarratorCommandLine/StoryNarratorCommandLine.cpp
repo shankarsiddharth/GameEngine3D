c74c789d7430240bee52fb31ec75bf48fbbe1537
Shankar Siddharth K A <7028991+shankarsiddharth@users.noreply.github.com>
2022-11-16 03:02:03 -0700

    Add Command Line Utility and Test Cases

---
+++ Engine/StoryNarratorCommandLine/StoryNarratorCommandLine.cpp
@@ -0,0 +1,61 @@
+// NarratorRuntime.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <Engine/StoryNarrator/Includes.h>
+#include <iostream>
+#include <random>
+
+int main()
+{
+
+	Narrator::Runtime::Story parsedStory = Narrator::Runtime::Story::Parse("./TestScripts/story_test.txt", "./TestScripts/story_test.json");
+
+	if (parsedStory.IsValid())
+	{
+		Narrator::Runtime::Story story;
+		story.FromJSONFile("./TestScripts/story_test.json");
+
+		while (story.canRead())
+		{
+			while (story.canRead())
+			{
+				std::string dialogue = story.Read();
+				if (!dialogue.empty())
+				{
+					std::cout << dialogue << std::endl;
+				}
+			}
+
+			std::vector<std::string> choices = story.GetChoices();
+			size_t choiceCount = choices.size();
+			if (choiceCount > 0)
+			{
+				size_t choiceIndex = 0;
+				for (const std::string& choiceText : choices)
+				{
+					std::cout << "Choice Index: " << choiceIndex << "\t" << choiceText << std::endl;
+					choiceIndex++;
+				}
+
+				std::random_device rd;  //Will be used to obtain a seed for the random number engine
+				std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+				std::uniform_int_distribution<size_t> distrib(0, (choiceCount - 1));
+
+				auto selectChoiceIndex = distrib(gen);
+				std::cout << "Selecting Choice Index: " << selectChoiceIndex << std::endl;
+				story.SelectChoice((std::uint32_t)selectChoiceIndex);
+				// 				story.SelectChoice(0);
+			}
+			else
+			{
+				//End of Story
+				break;
+			}
+		}
+
+		story.Read();
+
+	}
+
+
+	return 0;
+}
---
+++ Engine/StoryNarratorCommandLine/TestScripts/test_cases/correct_story.ink
@@ -0,0 +1,39 @@
+Hi
+
+How are you?
+	->Scene1
+
+=== Scene1 ===
+
+Starting Dialogue of the Scene1
+
+Make a choice:
+
+* Your Choice 1
+	Choice Dialogue 1
+	-> Scene2
+
+* Your Choice 2
+	Choice Dialogue 2
+	->END
+
+* Your Choice 3
+	Choice Dialogue 3
+	-> Scene2
+
+* Your Choice 4
+	Choice Dialogue 4
+	->DONE
+
+They are falling.
+
+=== Scene2 ===
+Starting Dialogue of the Scene2
+->end_scene
+
+=== Scene3 ===
+More dialogue for Scene3
+->END
+
+=== end_scene ===
+->END
